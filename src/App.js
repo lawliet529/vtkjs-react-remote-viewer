b120847a1be99f9a4d796ca7400c452e6d2d91e1
lawliet529 <85656044+lawliet529@users.noreply.github.com>
2024-05-07 13:50:28 +0700

    Use dark mode

--- src/App.js
+++ src/App.js
@@ -26,6 +26,15 @@ import {
 } from "@mui/icons-material";
 import RemoteRenderView from "./components/RemoteRenderingView";
 
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import CssBaseline from '@mui/material/CssBaseline';
+
+const darkTheme = createTheme({
+  palette: {
+    mode: "dark",
+  },
+});
+
 function CustomTabPanel(props) {
   const { children, value, index, ...other } = props;
 
@@ -70,96 +79,99 @@ function App() {
   };
 
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static" color="inherit">
-        <Toolbar>
-          <img src={logo} alt="logo" className="logo"></img>
-          <Typography variant="h6" sx={{ flexGrow: 1 }}>
-            Application
-          </Typography>
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static" color="inherit">
+          <Toolbar>
+            <img src={logo} alt="logo" className="logo"></img>
+            <Typography variant="h6" sx={{ flexGrow: 1 }}>
+              Application
+            </Typography>
 
-          <Switch checked={showing} onChange={handleChange} />
-          <IconButton onClick={resetCamera}>
-            <CameraAlt />
-          </IconButton>
-        </Toolbar>
-        <LinearProgress sx={{ opacity: !!busy ? 1 : 0 }} />
-      </AppBar>
-      <Box className="appContent">
-        <div
-          style={{
-            display: "flex",
-            flexGrow: 1,
-            position: "relative",
-            width: "100%",
-            height: "100%",
-          }}
-        >
-          <Box>
-            <Tabs
-              orientation="vertical"
-              value={tab}
-              onChange={handleTabChange}
-              sx={{ borderRight: 1, borderColor: "divider" }}
-            >
-              <Tab icon={<ViewColumn />} />
-              <Tab icon={<AutoAwesomeMosaic />} />
-              <Tab icon={<Landscape />} />
-            </Tabs>
-          </Box>
-          <CustomTabPanel value={tab} index={0} style={{ width: "100%" }}>
-            {showing && (
-              <Grid container style={{ width: "100%", height: "600px" }}>
-                <Grid item xs={4} sx={{ position: "relative" }}>
-                  <RemoteRenderView client={client} viewId="1" />
-                </Grid>
-                <Grid item xs={4} sx={{ position: "relative" }}>
-                  <RemoteRenderView client={client} viewId="2" />
-                </Grid>
-                <Grid item xs={4} sx={{ position: "relative" }}>
-                  <RemoteRenderView client={client} viewId="3" />
-                </Grid>
-              </Grid>
-            )}
-          </CustomTabPanel>
-          <CustomTabPanel value={tab} index={1} style={{ width: "100%" }}>
-            {showing && (
-              <Grid container style={{ width: "100%", height: "600px" }}>
-                <Grid item xs={6} sx={{ position: "relative" }}>
-                  <RemoteRenderView client={client} viewId="1" />
-                </Grid>
-                <Grid
-                  item
-                  container
-                  direction="column"
-                  xs={6}
-                  sx={{ position: "relative" }}
-                >
-                  <Grid item xs={6} sx={{ position: "relative" }}>
+            <Switch checked={showing} onChange={handleChange} />
+            <IconButton onClick={resetCamera}>
+              <CameraAlt />
+            </IconButton>
+          </Toolbar>
+          <LinearProgress sx={{ opacity: !!busy ? 1 : 0 }} />
+        </AppBar>
+        <Box className="appContent">
+          <div
+            style={{
+              display: "flex",
+              flexGrow: 1,
+              position: "relative",
+              width: "100%",
+              height: "100%",
+            }}
+          >
+            <Box>
+              <Tabs
+                orientation="vertical"
+                value={tab}
+                onChange={handleTabChange}
+                sx={{ borderRight: 1, borderColor: "divider" }}
+              >
+                <Tab icon={<ViewColumn />} />
+                <Tab icon={<AutoAwesomeMosaic />} />
+                <Tab icon={<Landscape />} />
+              </Tabs>
+            </Box>
+            <CustomTabPanel value={tab} index={0} style={{ width: "100%" }}>
+              {showing && (
+                <Grid container style={{ width: "100%", height: "600px" }}>
+                  <Grid item xs={4} sx={{ position: "relative" }}>
+                    <RemoteRenderView client={client} viewId="1" />
+                  </Grid>
+                  <Grid item xs={4} sx={{ position: "relative" }}>
                     <RemoteRenderView client={client} viewId="2" />
                   </Grid>
-                  <Grid item xs={6} sx={{ position: "relative" }}>
+                  <Grid item xs={4} sx={{ position: "relative" }}>
                     <RemoteRenderView client={client} viewId="3" />
                   </Grid>
                 </Grid>
-              </Grid>
-            )}
-          </CustomTabPanel>
-          <CustomTabPanel value={tab} index={2}>
-            <Box
-              component="img"
-              sx={{
-                width: "auto",
-                height: "auto",
-                maxWidth: 700,
-                maxHeight: 600,
-              }}
-              src="https://source.unsplash.com/random"
-            />
-          </CustomTabPanel>
-        </div>
-      </Box>
-    </Box>
+              )}
+            </CustomTabPanel>
+            <CustomTabPanel value={tab} index={1} style={{ width: "100%" }}>
+              {showing && (
+                <Grid container style={{ width: "100%", height: "600px" }}>
+                  <Grid item xs={6} sx={{ position: "relative" }}>
+                    <RemoteRenderView client={client} viewId="1" />
+                  </Grid>
+                  <Grid
+                    item
+                    container
+                    direction="column"
+                    xs={6}
+                    sx={{ position: "relative" }}
+                  >
+                    <Grid item xs={6} sx={{ position: "relative" }}>
+                      <RemoteRenderView client={client} viewId="2" />
+                    </Grid>
+                    <Grid item xs={6} sx={{ position: "relative" }}>
+                      <RemoteRenderView client={client} viewId="3" />
+                    </Grid>
+                  </Grid>
+                </Grid>
+              )}
+            </CustomTabPanel>
+            <CustomTabPanel value={tab} index={2}>
+              <Box
+                component="img"
+                sx={{
+                  width: "auto",
+                  height: "auto",
+                  maxWidth: 700,
+                  maxHeight: 600,
+                }}
+                src="https://source.unsplash.com/random"
+              />
+            </CustomTabPanel>
+          </div>
+        </Box>
+      </Box>{" "}
+    </ThemeProvider>
   );
 }
 
